7b18c608094365027654adb7f7340ef183fac275
Vinicius <57239647+Vindemoto@users.noreply.github.com>
2026-09-15 19:10:42 -0300

    Update ministraCursoTecnico.sql

--- atividade_1/ministraCursoTecnico.sql
+++ atividade_1/ministraCursoTecnico.sql
@@ -39,13 +39,15 @@ insert into curso values(2, 'Nutrição', '67.99', '5 dias');
 insert into curso values(3, 'Administração', '99.99', '4 sábados');
 insert into curso values(4, 'Manutenção de Hardware', '119.99', '10 noites');
 
-insert into instrutores values(1, 'Hellena', 'Rua Muito Real Nº 67', 11 96761-3919, 'Analise de Sistemas')
-insert into instrutores values(2, 'Sibílio', 'Rua Mais Real Nº 212', 11 93215-8912, 'Analise de Sistemas')
-insert into instrutores values(3, '', 'Rua Existente Nº 90', 11 96761-3919, 'Analise de Sistemas')
-insert into instrutores values(4, '', 'Rua Muito Real Nº 78', 11 96761-3919, 'Nutrição')
-insert into instrutores values(5, '', 'Rua Muito Real Nº 3234', 11 96761-3919, 'Nutrição')
-insert into instrutores values(6, '', 'Rua Muito Real Nº 12', 11 96761-3919, 'Nutrição')
-insert into instrutores values(7, 'Maria Hassuni', 'Rua  Nº 39', 11 96761-3919, 'Manutenção de Hardware')
-insert into instrutores values(8, '', 'Rua Muito Real Nº 5466', 11 96761-3919, 'Manutenção de Hardware')
+insert into instrutores values(1, 'Hellena', 'Rua Muito Real Nº 67', 11 96761-3919, 'Analise de Sistemas');
+insert into instrutores values(2, 'Sibílio', 'Rua Mais Real Nº 212', 11 93215-8912, 'Analise de Sistemas');
+insert into instrutores values(3, '', 'Rua Existente Nº 90', 11 96761-3919, 'Analise de Sistemas');
+insert into instrutores values(4, '', 'Rua Muito Real Nº 78', 11 96761-3919, 'Nutrição');
+insert into instrutores values(5, '', 'Rua Muito Real Nº 3234', 11 96761-3919, 'Nutrição');
+insert into instrutores values(6, '', 'Rua Muito Real Nº 12', 11 96761-3919, 'Nutrição');
+insert into instrutores values(7, 'Maria Hassuni', 'Rua  Nº 39', 11 96761-3919, 'Manutenção de Hardware');
+insert into instrutores values(8, '', 'Rua Muito Real Nº 5466', 11 96761-3919, 'Manutenção de Hardware');
+
+
 
 select * from curso;
